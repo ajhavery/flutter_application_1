0b708341b4f5db80625496a444bafcbe66764879
Ankur Jhavery <ajhavery@gmail.com>
2022-08-01 16:12:17 +0530

    chapter 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: const HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: const Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
