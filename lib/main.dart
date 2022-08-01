e874df588aa8682af43831217070c615f738b233
Ankur Jhavery <ajhavery@gmail.com>
2022-08-01 17:13:39 +0530

    chapter 3 start

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,42 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Container(
-        color: Colors.red,
-        child: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.0,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(0, 2),
+              )
+            ],
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.red,
+                Colors.orange,
+                Colors.yellow,
+              ],
+            ),
+            border: Border.all(
+              color: Colors.grey,
+              width: 1,
             ),
           ),
+          child: const Text('Hello World',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
         ),
       ),
     );
